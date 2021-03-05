e5b81c445c180af8f1ca35964528ef32101020cd
Giulia2Gargano <giulia2gargano@gmail.com>
2021-03-05 15:37:23 +0100

    Primo commit

---
+++ archivioClienti-ill-server/src/main/java/it/sirfin/archivioClientiillserver/ArchivioClientiIllServerApplication.java
@@ -0,0 +1,13 @@
+package it.sirfin.archivioClientiillserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchivioClientiIllServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchivioClientiIllServerApplication.class, args);
+	}
+
+}
